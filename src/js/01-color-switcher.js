24d25633e6c518f97c101128f79dd60d9c1bbbad
AleksandrSoiko <farshchef777@gmail.com>
2022-12-29 18:26:16 +0000

    color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+const bodyDocumentRef = document.querySelector('body');
+const buttonStartChangeThemeRef = document.querySelector('[data-start]');
+const buttonStopChangeThemeRef = document.querySelector('[data-stop]');
 
+buttonStartChangeThemeRef.addEventListener('click', () => {
+  const setIntervalId = setInterval(() => {
+    bodyDocumentRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStartChangeThemeRef.disabled = true;
+  buttonStopChangeThemeRef.addEventListener('click', () => {
+    clearInterval(setIntervalId);
+    buttonStartChangeThemeRef.disabled = false;
+  });
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
